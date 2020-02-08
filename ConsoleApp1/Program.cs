f34558a8ebc2f3a668090a9f229bc8ab4ec0c927
Wojciech Bijos <m4574h@gmail.com>
2020-02-09 00:48:43 +0100

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GradeBook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] numbers = new[] { 12.7, 10.3, 6.11, 4.1 };
+            var grades = new List<double>() { 12.7, 10.3, 6.11, 4.1 };
+            grades.Add(56.1);
+
+
+            var result = 0.0;
+            foreach (double number in grades)
+            {
+                result += number;
+            }
+            result /= grades.Count;
+
+            Console.WriteLine($"The Avrage grade is {result:N1}");
+
+
+            if (args.Length > 0)
+            {
+                Console.WriteLine("Hello " + args[0] + "!");
+                Console.WriteLine($"Hello {args[0]}!");
+            }
+            else
+            {
+                Console.WriteLine("Hello!");
+            }
+        }
+    }
+}
